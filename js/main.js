35bec69b59540500bdfad3505d66dd8fea328431
Marcos A. T <32946589+Tenebraw@users.noreply.github.com>
2023-11-14 19:20:57 -0300

    Implementacion de clases.
    Modificacion dela estructura original, ahora implemente dos clases, HistoricalRates y CurrencyConverter que se encargan de manejar cada entidad.

--- js/main.js
+++ js/main.js
@@ -3,47 +3,78 @@ const ratesForm = $("#rates");
 
 "#daterate".valueAsDate = new Date();
 
-ratesForm.on("submit", (e) => {
+class HistoricalRates {
+  constructor(baseCurrency, date) {
+    this.baseCurrency = baseCurrency;
+    this.date = date;
+    this.rates = {};
+  }
+
+  async fetchRates() {
+    try {
+      const response = await fetch(`https://api.frankfurter.app/${this.date}?from=${this.baseCurrency}`);
+      const data = await response.json();
+      this.rates = data.rates;
+    } catch (error) {
+      console.error("Failed to fetch historical rates", error);
+    }
+  }
+}
+
+class CurrencyConverter {
+  constructor(fromCurrency, toCurrency, amount) {
+    this.fromCurrency = fromCurrency;
+    this.toCurrency = toCurrency;
+    this.amount = amount;
+    this.result = null;
+  }
+
+  async convert() {
+    if (this.fromCurrency === this.toCurrency) {
+      this.result = `${this.amount} ${this.fromCurrency} = ${this.amount} ${this.toCurrency}`;
+    } else {
+      try {
+        const response = await fetch(`https://api.frankfurter.app/latest?amount=${this.amount}&from=${this.fromCurrency}&to=${this.toCurrency}`);
+        const data = await response.json();
+        this.result = `${this.amount} ${this.fromCurrency} = ${data.rates[this.toCurrency]} ${this.toCurrency}`;
+      } catch (error) {
+        console.error("Failed to convert currency", error);
+      }
+    }
+  }
+}
+
+ratesForm.on("submit", async (e) => {
   e.preventDefault();
-  let optionRate = $("#ratesoptions").val();
-  let dateRate = $("#daterate").val();
+  const optionRate = $("#ratesoptions").val();
+  const dateRate = $("#daterate").val();
 
-  fetch(`https://api.frankfurter.app/${dateRate}?from=${optionRate}`)
-    .then((respuesta) => respuesta.json())
-    .then((respuesta) => {
-      $("tbody").html("");
-      $("#tablerates").show();
-      $(".contenedortabla").show();
+  const historicalRates = new HistoricalRates(optionRate, dateRate);
+  await historicalRates.fetchRates();
 
-      $.each(respuesta.rates, function (moneda) {
+  $("tbody").html("");
+  $("#tablerates").show();
+  $(".contenedortabla").show();
+  $.each(historicalRates.rates, function (moneda) {
         $("tbody").append(
-          `<tr><th>.</th><td>${moneda}</td><td>${respuesta.rates[moneda]}</td></tr>`
+          `<tr><th>.</th><td>${moneda}</td><td>${historicalRates.rates[moneda]}</td></tr>`
         );
       });
     })
-    .catch((error) => console.error("Fallo!", error));
-});
 
-convertForm.on("submit", (e) => {
+
+convertForm.on("submit", async (e) => {
   e.preventDefault();
-  let fromConvert = $("#fromoptions").val();
-  let toConvert = $("#tooptions").val();
-  let amountConvert = $("#amount").val();
+  const fromConvert = $("#fromoptions").val();
+  const toConvert = $("#tooptions").val();
+  const amountConvert = $("#amount").val();
 
-  if (fromConvert === toConvert) {
-    $("#convertresult").text("1");
-  } else {
-    fetch(
-      `https://api.frankfurter.app/latest?amount=${amountConvert}&from=${fromConvert}&to=${toConvert}`
-    )
-      .then((respuesta) => respuesta.json())
-      .then((respuesta) => {
-        $("#convertresult").text(
-          `${amountConvert} ${fromConvert} = ${respuesta.rates[toConvert]} ${toConvert}`
+  const currencyConverter = new CurrencyConverter(fromConvert, toConvert, amountConvert);
+  await currencyConverter.convert();
+
+  $("#convertresult").text(
+          `${currencyConverter.result}`
         );
-      })
-      .catch((error) => console.error("Fallo!", error));
-  }
 });
 
 function isEmptyRates() {
@@ -51,6 +82,7 @@ function isEmptyRates() {
   let selectElement = $("[name=currency]");
   let optionValues = [...selectElement[0].options].map((o) => o.value);
   optionValues.shift();
+
 
   if (!optionValues.includes(optionRate)) {
     $("#ratesoptions").css("border", "2px solid red");
